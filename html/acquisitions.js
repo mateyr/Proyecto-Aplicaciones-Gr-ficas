fb5163a132ac17584acb6f81536d02e275db182c
MateyR <rodianmatey@gmail.com>
2023-07-07 09:29:07 -0600

    fix: Test chart.js

---
+++ html/acquisitions.js
@@ -0,0 +1,37 @@
+
+import Chart from '../node_modules/chart.js/auto';
+
+  console.log(Chart);
+
+(async function() {
+  const data = [
+    { mes: 'Enero', count: 10 },
+    { mes: 'Febrero', count: 20 },
+    { mes: 'Marzo', count: 15 },
+    { mes: 'Abril', count: 25 },
+    { mes: 'Mayo', count: 22 },
+    { mes: 'Junio', count: 30 },
+  ];
+
+  new Chart(
+    document.getElementById('acquisitions'),
+    {
+      type: 'bar',
+      data: {
+        labels: data.map(row => row.mes),
+        datasets: [
+          {
+            label: 'Ventas por Mes',
+            data: data.map(row => row.count)
+          }
+        ]
+      }
+    }
+  );
+})();
+
+
+
+
+
+
